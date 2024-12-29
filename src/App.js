620204a3be5527248f42d389d28ef621071650ce
dtrup <dumitrutpop@gmail.com>
2024-12-29 12:40:25 +0100

    Initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css'; // Import the CSS file
+
+function App() {
+  return (
+      <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-bold text-blue-500">
+          Hello, Learning Platform!
+        </h1>
+      </div>
+  );
+}
+
+export default App;
